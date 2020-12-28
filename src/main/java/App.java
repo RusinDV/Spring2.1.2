c873ca6119b57f85d16dc1c5240dcb6787dd3900
Dmitry <dtraider@mail.ru>
2020-12-28 11:08:21 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +9,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean.getName());
+        Cat catbean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println();
+        System.out.println("HelloWorld = "+ bean.equals(bean2));
+        System.out.println("Cat = " + catbean.equals(catbean2));
+
     }
 }
